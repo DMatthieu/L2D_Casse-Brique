189dd8ad382a139e30d28a63565870d386734aab
DMatthieu <matt.delporte6@gmail.com>
2019-08-02 23:22:03 +0200

    Base Architecture projet

---
+++ main.lua
@@ -0,0 +1,23 @@
+--Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no');
+
+--love.graphics.setDefaultFilter( "nearest" )
+
+
+
+function love.load()
+    --DIMENSIONS DE LA FENETRE
+    largeur = love.graphics.getWidth();
+    hauteur = love.graphics.getHeight();
+
+end
+
+function love.update(dt)
+
+    
+
+end
+
+function love.draw()
+    
+end
