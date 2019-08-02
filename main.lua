c9c1b26e8a91816c4e6047a12f5143f590cc0028
DMatthieu <matt.delporte6@gmail.com>
2019-08-03 00:03:53 +0200

    Add Debug Provider + Ball & Paddle follow Mouse before game launch

--- main.lua
+++ main.lua
@@ -21,12 +21,28 @@ function love.load()
         ball.width = 20
         ball.x = pad.x + (pad.width / 2)
         ball.y = pad.y - (ball.width / 2)
+        ball.stick = true
+
+    mousePos = {}
+        mousePos.x, mousePos.y = 0    
 
 end
 
 function love.update(dt)
 
+    --Update Mouse Pos
+    mousePos.x, mousePos.y = love.mouse.getPosition()
+
+    --update paddle position on X-Axis
+    pad.x = mousePos.x
+
+    --Position of the Ball on screen
+    if ball.stick  then
+        ball.x = pad.x + (pad.width / 2)
+        ball.y = pad.y - (ball.width / 2)
+    else
     
+    end
 
 end
 
@@ -34,9 +50,13 @@ function love.draw()
     --Draw paddle
     love.graphics.rectangle("fill", pad.x, pad.y, pad.width, pad.height)
     
+    --Draw ball
     love.graphics.setColor(255,0,0)
     love.graphics.circle("fill", ball.x, ball.y, ball.width / 2)
     love.graphics.reset()
+
+    PrintDebug()
+
 end
 
 
@@ -47,4 +67,12 @@ function love.keypressed(key)
     if key=="escape" then
         love.event.quit()
     end  
+end
+
+function PrintDebug() 
+    --Paddle informations
+    love.graphics.print("Pad.x: "..pad.x, 850, 10, 0, 1.5, 1.5)
+    love.graphics.print("Pad.y: "..pad.y, 850, 30, 0, 1.5, 1.5)
+    love.graphics.print("Mouse.x: "..mousePos.x, 850, 50, 0, 1.5, 1.5)
+    love.graphics.print("Mouse.y: "..mousePos.y, 850, 70, 0, 1.5, 1.5)
 end
