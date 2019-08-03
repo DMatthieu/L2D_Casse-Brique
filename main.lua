0e22dc1eff00a5ded99820334383dc5de46ea7c6
DMatthieu <matt.delporte6@gmail.com>
2019-08-04 00:59:43 +0200

    Add DrawLevel() function !!!! :D

--- main.lua
+++ main.lua
@@ -26,8 +26,22 @@ function love.load()
         ball.vy = -2
 
     mousePos = {}
-        mousePos.x, mousePos.y = 0    
+        mousePos.x, mousePos.y = 0  
+        
+    
 
+    level_1 = {
+                {1,1,1,1,1,1,1,1,1,1},
+                {1,1,1,1,1,1,1,1,1,1},
+                {1,1,1,1,1,1,1,1,1,1},
+                {1,1,1,1,1,1,1,1,1,1}
+            }
+
+    tile = {}
+        tile.width = (97 * (largeur / #level_1[1])) / 100
+        tile.height = ball.width
+         
+        tile.offset = 2
 
 end
 
@@ -61,12 +75,13 @@ function love.draw()
 
     PrintDebug()
 
-   
-
+    DrawLevel(level_1)
+    
 end
 
 --Callback checking if mouse is clicked
 function love.mousepressed(x, y, button, istouch, presses)
+    --LAUNCH THE BALL IF MOUSECLICK
     if button == 1 and ball.stick == true then
         ball.stick = false
         --WARNING: This callback can call whatever love function
@@ -75,7 +90,17 @@ function love.mousepressed(x, y, button, istouch, presses)
     end
 end
 
-
+function DrawLevel(pLevel)
+    for i=1,#pLevel,1 do
+        local tile_y = 2 + ((i - 1) * (tile.height + tile.offset))
+        for j=1,#pLevel[i],1 do
+            local tile_x = (j - 1) * (largeur / #pLevel[i])
+            love.graphics.setColor(255,0,255)
+            love.graphics.rectangle("fill", tile_x, tile_y, tile.width, tile.height)
+            love.graphics.reset()
+        end
+    end
+end
 
 --Permit Game Extinction
 function love.keypressed(key)
@@ -90,4 +115,6 @@ function PrintDebug()
     love.graphics.print("Pad.y: "..pad.y, 850, 30, 0, 1.5, 1.5)
     love.graphics.print("Mouse.x: "..mousePos.x, 850, 50, 0, 1.5, 1.5)
     love.graphics.print("Mouse.y: "..mousePos.y, 850, 70, 0, 1.5, 1.5)
+
+    love.graphics.print("#level_1[1] : "..#level_1[1], 850, 90, 0, 1.5, 1.5)
 end
